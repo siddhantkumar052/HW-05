b85991b798238830447c356f630058c71c01cfec
Siddhant Kumar <siddhantkumar052@gmail.com>
2022-10-12 21:27:47 -0400

    refactor

--- test_triangle.py
+++ test_triangle.py
@@ -8,56 +8,75 @@ import unittest
 from triangle_program import classify_triangle
 
 
-class TestTriangles(unittest.TestCase):
-    def testInvalidInput1(self):
+class Testtriangles(unittest.TestCase):
+    'This class contains all the tests for the classify triangle program'
+
+    def test_invalid_input_1(self):
+        'This is test case 1'
         self.assertEqual(classify_triangle(3, 4, 0), 'InvalidInput')
 
-    def testInvalidInput2(self):
+    def test_invalid_input_2(self):
+        'This is test case 2'
         self.assertEqual(classify_triangle(-1, -4, 5), 'InvalidInput')
 
-    def testInvalidInput3(self):
+    def test_invalid_input_3(self):
+        'This is test case 3'
         self.assertNotEqual(classify_triangle(3, 4, 5), 'InvalidInput')
 
-    def testNotATriangle4(self):
+    def test_not_a_triangle_4(self):
+        'This is test case 4'
         self.assertEqual(classify_triangle(10, 2, 3), 'NotATriangle')
 
-    def testNotATriangle5(self):
+    def test_not_a_triangle_5(self):
+        'This is test case 5'
         self.assertNotEqual(classify_triangle(3, 4, 5), 'NotATriangle')
 
-    def testNotATriangle6(self):
+    def test_not_a_triangle_6(self):
+        'This is test case 6'
         self.assertNotEqual(classify_triangle(5, 5, 8), 'NotATriangle')
 
-    def testRightTriangle7(self):
+    def test_right_triangle_7(self):
+        'This is test case 7'
         self.assertEqual(classify_triangle(3, 4, 5), 'Right')
 
-    def testRightTriangle8(self):
+    def test_right_triangle_8(self):
+        'This is test case 8'
         self.assertNotEqual(classify_triangle(11, 11, 10), 'Right')
 
-    def testEquilateralTriangle9(self):
+    def test_equilateral_triangle_9(self):
+        'This is test case 9'
         self.assertEqual(classify_triangle(5, 5, 5), 'Equilateral')
 
-    def testEquilateralTriangles10(self):
+    def test_equilateral_triangle_10(self):
+        'This is test case 10'
         self.assertEqual(classify_triangle(4, 4, 4), 'Equilateral')
 
-    def testEquilateralTriangles11(self):
+    def test_equilateral_triangle_11(self):
+        'This is test case 11'
         self.assertNotEqual(classify_triangle(4, 8, 10), 'Equilateral')
 
-    def testIsoscelesTriangle12(self):
+    def test_isosceles_triangle_12(self):
+        'This is test case 12'
         self.assertEqual(classify_triangle(4, 4, 5), 'Isosceles')
 
-    def testIsoscelesTriangle13(self):
+    def test_isosceles_triangle_13(self):
+        'This is test case 13'
         self.assertEqual(classify_triangle(5, 5, 8), 'Isosceles')
 
-    def testIsoscelesTriangle14(self):
+    def test_isosceles_triangle_14(self):
+        'This is test case 14'
         self.assertNotEqual(classify_triangle(10, 10, 10), 'Isosceles')
 
-    def testScaleneTriangle15(self):
+    def test_scalene_triangle_15(self):
+        'This is test case 15'
         self.assertEqual(classify_triangle(4, 8, 10), 'Scalene')
 
-    def testScaleneTriangle16(self):
+    def test_scalene_triangle_16(self):
+        'This is test case 16'
         self.assertEqual(classify_triangle(5, 9, 11), 'Scalene')
 
-    def testScaleneTriangle17(self):
+    def test_scalene_triangle_17(self):
+        'This is test case 17'
         self.assertNotEqual(classify_triangle(10, 10, 10), 'Scalene')
 
 
--- triangle_program.py
+++ triangle_program.py
@@ -6,11 +6,10 @@ HW 05 - Static Code Analysis
 """
 
 
-def classify_triangle(a, b, c):
+def classify_triangle(side_a, side_b, side_c):
     """
     This function returns a string with the type of triangle from three integer values
-    corresponding to the lengths of the three sides of the Triangle.
-    
+    corresponding to the lengths of the three sides of the Triangle
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
@@ -20,23 +19,23 @@ def classify_triangle(a, b, c):
     """
     # verify that all 3 inputs are integers
     # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
+    if not (isinstance(side_a, int) and isinstance(side_b, int) and isinstance(side_c, int)):
         return 'InvalidInput'
 
-    if a <= 0 or b <= 0 or c <= 0:
+    if side_a <= 0 or side_b <= 0 or side_c <= 0:
         return 'InvalidInput'
 
     # Check for Triangle Property : The sum of the length of the two sides
     # of a triangle is greater than the length of the third side.
-    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+    if (side_a >= (side_b + side_c)) or (side_b >= (side_a + side_c)) or (side_c >= (side_a + side_b)):
         return 'NotATriangle'
 
     # After All checks above, We can Classify the Triangles
-    if a == b and b == c:
+    if side_a == side_b and side_b == side_c:
         return 'Equilateral'
-    elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or ((b ** 2) + (c ** 2)) == (a ** 2):
+    if ((side_a ** 2) + (side_b ** 2)) == (side_c ** 2) or ((side_a ** 2) + (side_c ** 2)) == (side_b ** 2) or ((side_b ** 2) + (side_c ** 2)) == (side_a ** 2):
         return 'Right'
-    elif (a != b) and (b != c) and (a != c):
+    if (side_a != side_b) and (side_b != side_c) and (side_a != side_c):
         return 'Scalene'
     else:
         return 'Isosceles'
